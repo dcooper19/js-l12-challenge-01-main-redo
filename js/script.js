463b6018f06ac9e43534abe73766a9c156807ed8
Dianne Cooper <shewcoop@yahoo.com>
2023-03-27 15:56:40 -0400

    Code to reveal a random image, section 6, lesson 5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
+//code for lesson 12 - 5-6
+
 const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
+ const authorSpan = document.querySelector(".author");
+ const imgDiv = document.querySelector(".image-container");
+ const img = document.querySelector(".img");
 
-const getImage = async function () {
-	const res = await fetch("https://picsum.photos/v2/list?limit=100");
-	const images = await res.json();
-	console.log(images);
-};
+ const getImage = async function () {
+   const res = await fetch("https://picsum.photos/v2/list?limit=100");
+   const images = await res.json();
+   selectRandomImage(images);
+ };
 
-getImage();
+//getImage(); this was part of section 5 
+
+ const selectRandomImage = function (images) {
+   const randomIndex = Math.floor(Math.random() * images.length);
+   // console.log(randomIndex);
+   const randomImage = images[randomIndex];
+
+   // console.log(randomImage);
+   
+   displayImage(randomImage);
+ };
+
+ const displayImage = function (randomImage) {
+   const author = randomImage.author;
+   const imageAddress = randomImage.download_url;
+   authorSpan.innerText = author;
+   img.src = imageAddress;
+   imgDiv.classList.remove("hide");
+ };
+
+ button.addEventListener("click", function () {
+   getImage();
+ });
